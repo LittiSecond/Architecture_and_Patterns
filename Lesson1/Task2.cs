56e1947fd262978ede79a0f3e1abc80d8848e0c4
LittiSecond <breeze2882@yandex.ru>
2020-10-05 22:37:48 +0300

    Выполнено задание 2.

--- Lesson1/Task2.cs
+++ Lesson1/Task2.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Lesson1.Task2
 {
@@ -16,42 +16,68 @@ namespace Lesson1.Task2
 
         public static readonly string Address = Constants.Address;
         public static readonly string Format = Constants.Format;
-        private static void DummyFunc()
+
+        private static string DummyFunc()
         {
-            WriteToConsole("Петя", "школьный друг", 30);
+            return CreateString("Петя", "школьный друг", 30);
         }
 
-        private static void DummyFuncAgain()
+        private static string DummyFuncAgain()
         {
-            WriteToConsole("Вася", "сосед", 54);
+            return CreateString("Вася", "сосед", 54);
         }
 
-        private static void DummyFuncMore()
+        private static string DummyFuncMore()
         {
-            WriteToConsole("Николай", "сын", 4);
+            return CreateString("Николай", "сын", 4);
         }
 
-        private static void WriteToConsole(string name, string description,
+        private static string CreateString(string name, string description,
                                            int age)
         {
-            Console.WriteLine(Format, name, description, Address, age);
+            return string.Format(Format, name, description, Address, age);
+        }
+
+        private static void MakeAction(Func<string> action)
+        {
+            string methodName = action.Method.Name;
+            Console.WriteLine("Начало работы метода {0}", methodName);
+            Console.WriteLine(action());
+            Console.WriteLine("Окончание работы метода {0}", methodName);
+        }
+
+        private static List<Func<string>> GetActionSteps()
+        {
+            return new List<Func<string>>
+            {
+                 DummyFunc,
+                 DummyFuncAgain,
+                 DummyFuncMore
+            };
         }
 
         public static void MainTask2()
         {
-            Console.WriteLine("Начало работы метода DummyFunc");
-            DummyFunc();
-            Console.WriteLine("Окончание работы метода DummyFunc");
-
-            Console.WriteLine("Начало работы метода DummyFuncAgain");
-            DummyFuncAgain();
-            Console.WriteLine("Окончание работы метода DummyFuncAgain");
-
-            Console.WriteLine("Начало работы метода DummyFuncMore");
-            DummyFuncMore();
-            Console.WriteLine("Окончание работы метода DummyFuncMore");
+            List<Func<string>> actions = GetActionSteps();
+            foreach (var action in actions)
+            {
+                MakeAction(action);
+            }
 
             Console.ReadLine();
         }
+
+        // Считаю такой вариант лучше, а городить, то что сделано в двух методах выше, не нужно,
+        // так как оно сильно усложняет программу
+
+        //public static void MainTask2()
+        //{
+        //    MakeAction(DummyFunc);
+        //    MakeAction(DummyFuncAgain);
+        //    MakeAction(DummyFuncMore);
+
+        //    Console.ReadLine();
+        //}
+
     }
 }
