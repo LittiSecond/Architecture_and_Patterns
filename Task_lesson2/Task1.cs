3f54f10ccb0559d101d924d9d0222252a12a933b
LittiSecond <breeze2882@yandex.ru>
2020-10-08 08:47:56 +0300

    Переписан текст заданий из методички

---
+++ Task_lesson2/Task1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Task_lesson2.Task1
+{
+    /*
+     Задание 1. Применить принцип открытости / закрытости. Класс OrderRepository изначально был 
+    реализован для работы с заказами, хранящимися в MySQL. Но однажды нам потребовалось 
+    реализовать работу с данными о заказах, например, через API стороннего веб-сервиса. 
+    Какие изменения нам надо будет внести?
+     */
+
+    class Order
+    {
+        int orderId;
+        string Name;
+    }
+
+    class OrderRepository
+    {
+        public Order Load(int orderId) { return ... }
+        public void Save(Order order) { ... }
+        public void Update(Order order) { ... }
+        public void Delete(Order order) { ... }
+    }
+
+    class Task1
+    {
+    }
+
+}
---
+++ Task_lesson2/Task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Task_lesson2.Task2
+{
+    /*
+     2. Принцип разделения интерфейсов.Товары в магазине могут иметь промокод, 
+    скидку, у них есть цена, состояние и т. д. Если этот товар – одежда, для 
+    нее устанавливается материал, из которого она сделана, ее цвет и размер. 
+    Какие изменения нужно внести в код, чтобы описать произвольные товары 
+    (книги, автомобили и т. п.), соблюдая принципы SOLID? 
+     */
+
+    interface IItem
+    {
+        void SetDiscount(double discount);
+        void SetPromocode(string promocode);
+
+        void SetColor(Color color);
+        void SetSize(Size size);
+
+        void SetPrice(double price);
+    }
+
+    class Clothes : IItem
+    {
+        public void SetColor(Color color) { }
+        public void SetDiscount(double discount) { }
+        public void SetPrice(double price) { }
+        public void SetPromocode(string promocode) { }
+        public void SetSize(Size size) { }
+    }
+
+
+    class Task2
+    {
+    }
+}
