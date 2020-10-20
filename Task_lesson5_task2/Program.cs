09fdcc3003dfbb032777d3031178fc8f18316d4a
LittiSecond <breeze2882@yandex.ru>
2020-10-20 13:34:22 +0300

    Создан пустой проект

---
+++ Task_lesson5_task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_lesson5_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------");
+
+
+            Console.ReadKey();
+        }
+    }
+}
